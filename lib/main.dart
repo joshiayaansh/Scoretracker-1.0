9357a34cc280e5d0078a094b6261fee595eea14f
joshiayaansh <79388921+joshiayaansh@users.noreply.github.com>
2024-03-05 17:30:47 -0600

    new files

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Button Example"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Add your button onPressed logic here
+            print("Button pressed");
+          },
+          child: Text("Press Me"),
+        ),
+      ),
+    );
+  }
+}
